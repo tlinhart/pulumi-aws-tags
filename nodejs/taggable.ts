fc15d2d2b7bcab757ade219ed7a92f38e70b107d
Tomáš Linhart <pasmen@gmail.com>
2024-09-04 09:15:08 +0200

    Update to Node.js 18 baseline
    - https://github.com/microsoft/TypeScript/wiki/Node-Target-Mapping
    - https://www.typescriptlang.org/docs/handbook/modules/reference.html#the-module-compiler-option
    - https://www.typescriptlang.org/docs/handbook/modules/theory.html#the-module-output-format

--- nodejs/taggable.ts
+++ nodejs/taggable.ts
@@ -1,4 +1,4 @@
-import * as taggableResourceTypes from "./taggable_resource_types.json";
+import taggableResourceTypes from "./taggable_resource_types.json";
 
 /**
  * Return if the given resource type is a taggable AWS resource.
